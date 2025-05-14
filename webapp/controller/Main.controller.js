d52ec3107925bcc7827fe54fb9f2235f071c5ec2
shivam-asati <shivam-asati@users.noreply.github.com>
2025-05-14 10:53:57 +0000

    AI Generated (BTP-AI Forge)

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,31 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
-	"use strict";
-
-	return BaseController.extend("test.controller.Main", {
-		sayHello: function () {
-			MessageBox.show("Hello World!");
-		}
-	});
+sap.ui.define([
+  sap/ui/core/mvc/Controller,
+  sap/ui/model//JSONModel,
+  sap/m/MessageToast
+], function(Controller, JSONModel, MessageToast) {
+  use strict;
+  return Controller.extend(test.controller.Main, {
+    onInit: function() {
+      var oData = {
+        items: [
+          { title: Item 1 },
+          { title: Item 2 }
+        ]
+      };
+      var oModel = new JSONModel(oData);
+      this.getView().setModel(oModel);
+    },
+    onPress: function() {
+      var oInput = this.getView().byId(inputField);
+      var sValue = oInput.getValue();
+      if (sValue) {
+        MessageToast.show(You entered:  + sValue);
+      } else {
+        MessageToast.show(Please enter some text.);
+      }
+    },
+    handleError: function(oError) {
+      MessageToast.show(An error occurred:  + oError.message);
+    }
+  });
 });
